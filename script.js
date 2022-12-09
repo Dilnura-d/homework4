32157c63e5b0ddc163d3f53bda48a20282a6852a
Dilnura [FE16] <102959751+Dilnura-d@users.noreply.github.com>
2022-12-09 15:13:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+
+/*seil*/
+let num5 = 55.5;
+console.log(num5.toFixed()); 
+
+
+/*floor*/
+let num6 = 55.4;
+console.log(num6.toFixed());
+
+/*pow*/
+let num3 = prompt("type a num");
+let sqrtnum3 = num3 ** 2;
+console.log(sqrtnum3);
+
+/*floor*/ 
+ let number = 5.574
+ function floor() {
+     return ~number * -1 -1
+ }
+ console.log(floor(number))
+
+/*round*/
+ Math.round(2.7, 2.4) // 3, 2
+function round(num){
+    let result = num % 1;
+    return num-result+(result/1+1.5>>1)*1
+}
+console.log(round(2.7));
+
+/*sqrt*/
+ Math.sqrt(16, 2) // 4 (8, 3) // 2
+let x = 16
+let y = 2
+ function sqrt() {
+     return x  (1/b);
+}
+ sqrt()
+console.log(sqrt(x, y));
+
+let a = 8
+let n = 3
+function sqrt() {
+    return a ** (1/b);
+}
+sqrt()
+console.log(sqrt(x, n));
